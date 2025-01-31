b814330313c2e8bce0540e4d6c0f533447fc6c29
Tri27sen <trishasengupta27@gmail.com>
2025-02-01 00:51:47 +0530

    added components

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,18 +1,29 @@
-import {auth} from "../utils/auth"
+//import {auth} from "../utils/auth"
 import { redirect } from "next/navigation";
-// dashboard only for logined in user 
+import { requireUser } from "../utils/hooks";
+import { signOut } from "next-auth/react";
+// dashboard only for logged in user 
 //redirect to the login page
 //check the login
 export default async function DashboardRoute() {
-  const session = await auth();
   
-
+  const session = await requireUser();
+  console.log(session)
+  
   if(!session?.user){
     redirect("/")
   }
     return (
   <div>
     <h1> hello from dashboard route</h1>
+    <form
+    action ={async () => {
+      "use server";
+      await signOut();
+    }}
+    >
+      <button type = "submit"> Sign Out</button>
+    </form>
   </div>
   )
 }
