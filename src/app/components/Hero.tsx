5a9af7caa5656cf3f70e7e25aa9d5c05640acfe0
Tri27sen <trishasengupta27@gmail.com>
2025-03-06 19:14:11 +0530

    added landing page

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,14 +1,14 @@
 import { RainbowButton } from "@/components/ui/rainbow-button";
-import Image from "next/image";
+//import Image from "next/image";
 import Link from "next/link";
-import HeroImage from "@/public/hero.png";
+//import HeroImage from "@/public/hero.png";
 
 export function Hero() {
   return (
     <section className="relative flex flex-col items-center justify-center py-12 lg:py-20">
       <div className="text-center">
         <span className="text-sm text-primary font-medium tracking-tight bg-primary/10 px-4 py-2 rounded-full">
-          Introducing InvoiceMarshal 1.0
+          Introducing PAYSTREAM 
         </span>
         <h1 className="mt-8 text-4xl sm:text-6xl md:text-7xl lg:text-8xl font-semibold tracking-tighter">
           Invoicing made{" "}
@@ -18,7 +18,7 @@ export function Hero() {
         </h1>
 
         <p className="max-w-xl mx-auto mt-4 lg:text-lg text-muted-foreground">
-          Creating Invoices can be a pain! We at InvoiceMarshal make it super
+          Creating Invoices can be a pain! We at PAYSTREAM make it super
           easy for you to get paid in time!
         </p>
 
@@ -83,11 +83,7 @@ export function Hero() {
             </filter>
           </defs>
         </svg>
-        <Image
-          src={HeroImage}
-          alt="Hero image"
-          className="relative object-cover w-full border rounded-lg lg:rounded-2xl shadow-2xl"
-        />
+        
       </div>
     </section>
   );
