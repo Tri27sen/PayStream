5f32ec16a4b8400cc1934cc12ba0f563277dafde
Tri27sen <trishasengupta27@gmail.com>
2025-03-10 19:59:56 +0530

    added element

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
+//import Image from "next/image";
 import Link from "next/link";
-import Logo from "@/public/logo.png";
+//import Logo from "@/public/logo.png";
 //import { buttonVariants } from "@/components/ui/button";
 import { RainbowButton } from "@/components/ui/rainbow-button";
 
@@ -8,9 +8,9 @@ export function Navbar() {
   return (
     <div className="flex items-center justify-between py-5">
       <Link href="/" className="flex items-center gap-2">
-        <Image src={Logo} alt="Logo" className="size-10" />
+        
         <h3 className="text-3xl font-semibold">
-          Invoice<span className="text-blue-500">Marshal</span>
+          Pay<span className="text-blue-500">Stream</span>
         </h3>
       </Link>
       <Link href="/login">
